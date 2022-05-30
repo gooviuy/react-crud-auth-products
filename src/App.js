52821b3183b17cee4e1d0500d3a29101060c46c9
gooviuy <gooviuy@outlook.com>
2022-05-30 16:31:23 -0300

    first setup, primereact injected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css"; // primeng theme
+import "primereact/resources/primereact.min.css"; // core css
+import "primeicons/primeicons.css"; // icons
+
+import { Dialog } from "primereact/dialog";
+import { Button } from "primereact/button";
+import { useState } from "react";
 
 function App() {
+  const [state, setState] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Dialog visible={state} onHide={() => setState(false)}>
+        Hello
+      </Dialog>
+
+      <Button label="Show" onClick={() => setState(true)} />
     </div>
   );
 }
